5cf46f7c6fca88805c0666f22c77f7eeb174f8f9
RC-14 <61058098+RC-14@users.noreply.github.com>
2021-04-16 11:15:11 +0200

    new update checker to get rid of derecated warning

--- js/background.js
+++ js/background.js
@@ -4,24 +4,54 @@
  * -getting new URLs automatically
  */
 
-function getLatestVersion() {
-	var request = new XMLHttpRequest();
-	// get HTML of the GitHub page of the latest release with a synchronous XMLHTTPRequest
-	request.open("GET", "https://github.com/RC-14/StreamScript/releases/latest", false);
-	request.send(null);
+class UpdateChecker {
+	constructor(url, intervalTimeout = 1800000) {
+		this.url = url;
+		this.latestVersionString;
+		this.getLatestVersion();
 
-	var latest = request.responseText.split(/<\/?title>/g)[1].match(/v\d(\.\d+){2}/g)[0];
+		this.intervalTimeout = intervalTimeout;
+		this.intervalIDs = [];
+	}
 
-	return latest;
+	getLatestVersion() {
+		var promise = new Promise((resolve, reject) => {
+			var request = new XMLHttpRequest();
+			// get HTML of the GitHub page of the latest release
+			request.open("GET", this.url);
+			request.onreadystatechange = () => {
+				if (request.readyState === XMLHttpRequest.DONE) {
+					// get latest version from title
+					this.latestVersionString = request.responseText.split(/<\/?title>/g)[1].match(/v\d(\.\d+){2}/g)[0];
+					// check if the installed version is also the latest version on github (ignore check if this is a devBuild)
+					resolve(this.latestVersionString);
+				}
+			};
+			request.send();
+		});
+
+		return promise;
+	}
+
+	addInterval(intervalTimeout = this.intervalTimeout) {
+		this.intervalTimeout = intervalTimeout;
+		this.intervalIDs[this.intervalIDs.length] = setInterval(this.getLatestVersion, intervalTimeout);
+		return this.intervalIDs[this.intervalIDs.length - 1];
+	}
+
+	clearInterval(index) {
+		clearInterval(this.intervalIDs.splice(index, 1)[0]);
+	}
 }
+
+const updateChecker = new UpdateChecker("https://github.com/RC-14/StreamScript/releases/latest");
 
 chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
 	switch (request.msg) {
 		case "getLatestVersion":
 			console.log("getLatestVersion");
-			var latest = getLatestVersion();
-			console.log("latest: " + latest);
-			sendResponse(latest);
+			console.log("latest: " + updateChecker.latestVersionString);
+			sendResponse(updateChecker.latestVersionString);
 			break;
 
 		default:
