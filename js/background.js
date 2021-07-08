ce1a46ce5346b5e54f0168e9fc903d402a0b472b
RC-14 <61058098+RC-14@users.noreply.github.com>
2021-07-08 21:24:03 +0200

    v2.0.0
    rewrote the entire extension (not from the ground but almost)

--- js/background.js
+++ js/background.js
@@ -1,12 +1,7 @@
-/*
- * ideas for usage:
- * -resume to a cached time after getting a new URL
- * -getting new URLs automatically
- */
-
 class UpdateChecker {
 	constructor(url, intervalTimeout = 300000) {
 		this.url = url;
+		this.currentVersion = chrome.runtime.getManifest().version;
 		this.latestVersionString;
 		this.getLatestVersion();
 
@@ -34,27 +29,229 @@ class UpdateChecker {
 	}
 
 	addInterval(intervalTimeout = this.intervalTimeout) {
+		this.currentVersion = chrome.runtime.getManifest().version;
 		this.intervalTimeout = intervalTimeout;
 		this.intervalIDs[this.intervalIDs.length] = setInterval(this.getLatestVersion, intervalTimeout);
 		return this.intervalIDs[this.intervalIDs.length - 1];
 	}
 
-	clearInterval(index) {
+	clearInterval(index = 0) {
 		clearInterval(this.intervalIDs.splice(index, 1)[0]);
 	}
 }
 
+class InstructionsManager {
+	constructor(url, intervalTimeout) {
+		this.url = url;
+		this.localURL = chrome.runtime.getURL("instructions.json");
+		this.instructions = undefined;
+		this.useOfflineInstructions();
+		this.getLatestInstructions();
+
+		this.intervalTimeout = intervalTimeout;
+		this.intervalIDs = [];
+	}
+
+	generateAllWildcardDomains(domain) {
+		if (typeof domain !== "string") {
+			throw new Error("InstructionsManager.generateAllWildcardDomains: type of domain is not string");
+		} else if (!domain.match(/^([a-z0-9\-]+\.)*[a-z0-9]+$/gi)) {
+			throw new Error("InstructionsManager.generateAllWildcardDomains: domain is not a valid domain");
+		}
+		let result = [];
+		for (let i = 1; i < domain.split(".").length; i++) {
+			var tmpDomain = domain.split(".");
+
+			for (let j = 0; j < i; j++) tmpDomain[j] = "*";
+			tmpDomain = tmpDomain.join(".");
+
+			result.push(tmpDomain);
+		}
+		return result;
+	}
+
+	generateAllWildcardPaths(path) {
+		if (typeof path !== "string") {
+			throw new Error("InstructionsManager.generateAllWildcardPaths: type of path is not string");
+		} /* else if (!path.match(/^(\/[a–z0–9\-\._~!\$&'\(\)\*\+,;=:@%]+)+\/?$/gi)) {
+			// doesn't work - chars allowed in path (except /): a-z A-Z 0-9 - . _ ~ ! $ & ' ( ) * + , ; = : @ %
+			throw new Error("InstructionsManager.generateAllWildcardPaths: path contains forbidden characters");
+		}*/
+
+		path = path.replace(/\/$/g, "");
+
+		var tmp = path.split("/")[path.split("/").length - 1].split(".");
+		const suffix = tmp[tmp.length - 1] !== path.split("/")[path.split("/").length - 1] ? tmp[tmp.length - 1] : "";
+
+		let result = [];
+
+		if (suffix) {
+			for (let i = 0; i < path.replace(/^\//g, "").split("/").length; i++) {
+				var tmpPath = path.replace(/^\//g, "").split("/");
+
+				for (let j = 0; j <= i; j++) tmpPath[tmpPath.length - 1 - j] = "*";
+
+				tmpPath = "/" + tmpPath.join("/") + "." + suffix;
+
+				result.push(tmpPath);
+			}
+		}
+
+		for (let i = 0; i < path.replace(/^\//g, "").split("/").length; i++) {
+			var tmpPath = path.replace(/^\//g, "").split("/");
+
+			for (let j = 0; j <= i; j++) tmpPath[tmpPath.length - 1 - j] = "*";
+
+			tmpPath = "/" + tmpPath.join("/");
+
+			result.push(tmpPath);
+		}
+		console.log(result);
+
+		return result;
+	}
+
+	setInstructions(instructions) {
+		if (typeof instructions === "string") {
+			instructions = JSON.parse(instructions);
+		} else if (typeof instructions !== "object") {
+			throw new Error("InstructionsManager.setInstructions: type of instructions is not string or object");
+		}
+		this.instructions = instructions;
+	}
+
+	getInstrutionsForURL(url) {
+		function getKeyWithGenerator(obj, input, generator) {
+			let result = null;
+			if (obj[input]) {
+				result = input;
+			} else {
+				var generatedKeys = generator(input);
+				for (let i = 0; i < generatedKeys.length; i++) {
+					if (obj[generatedKeys[i]]) {
+						result = generatedKeys[i];
+						break;
+					}
+				}
+			}
+
+			while (typeof obj[result] === "string") {
+				result = obj[result];
+			}
+
+			if (typeof obj[result] !== "object") {
+				return null;
+			}
+
+			return result;
+		}
+
+		const protocol = url.split("://")[0];
+
+		const host = protocol !== "file" ? url.split("://")[1].match(/^([a-z0-9\-]+\.)*[a-z0-9\-]+/gi)[0] : "";
+
+		var tmp = url
+			.split(host ? host : ":///")[1]
+			.split("#")[0]
+			.split("?")[0]
+			.replace(/\/$/g, "");
+		const path = tmp.startsWith("/") ? tmp : "";
+
+		let instructions = null;
+
+		let domainKey = getKeyWithGenerator(this.instructions, host, this.generateAllWildcardDomains);
+		if (!domainKey) return instructions;
+
+		let pathKey = getKeyWithGenerator(this.instructions[domainKey], path, this.generateAllWildcardPaths);
+		if (!pathKey) return instructions;
+
+		instructions = this.instructions[domainKey][pathKey];
+
+		return instructions;
+	}
+
+	useOfflineInstructions() {
+		var promise = new Promise((resolve, reject) => {
+			var request = new XMLHttpRequest();
+			request.open("GET", this.localURL);
+			request.onreadystatechange = () => {
+				if (request.readyState === XMLHttpRequest.DONE) {
+					this.setInstructions(request.responseText);
+					resolve();
+				}
+			};
+			request.send();
+		});
+
+		return promise;
+	}
+
+	getLatestInstructions() {
+		var promise = new Promise((resolve, reject) => {
+			var request = new XMLHttpRequest();
+			request.open("GET", this.url);
+			request.onreadystatechange = () => {
+				let status = request.status;
+				if (request.readyState === XMLHttpRequest.HEADERS_RECEIVED) {
+					if (!(status >= 200 && status < 400)) {
+						request.abort();
+						reject();
+					}
+				} else if (request.readyState === XMLHttpRequest.DONE) {
+					if (status >= 200 && status < 400) {
+						this.setInstructions(request.responseText);
+						resolve();
+					}
+				}
+			};
+			request.send();
+		});
+
+		return promise;
+	}
+
+	addInterval(intervalTimeout = this.intervalTimeout) {
+		this.intervalTimeout = intervalTimeout;
+		this.intervalIDs[this.intervalIDs.length] = setInterval(this.getLatestInstructions, intervalTimeout);
+		return this.intervalIDs[this.intervalIDs.length - 1];
+	}
+
+	clearInterval(index = 0) {
+		clearInterval(this.intervalIDs.splice(index, 1)[0]);
+	}
+}
+
+class VideoManager {
+	constructor() {}
+}
+
 const updateChecker = new UpdateChecker("https://github.com/RC-14/StreamScript/releases/latest");
+const instructionsManager = new InstructionsManager("https://raw.githubusercontent.com/RC-14/StreamScript/main/instructions.json", 600000);
+const videoManager = new VideoManager();
+
+const messages = {};
+messages.getInstrutions = "getInstructions";
+messages.redirectToVideoSrc = "redirectToVideoSrc";
+messages.setLastTime = "setLastTime";
+messages.getLastTime = "getLastTime";
 
 chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
 	switch (request.msg) {
-		case "getLatestVersion":
-			console.log("getLatestVersion");
-			console.log("latest: " + updateChecker.latestVersionString);
-			sendResponse(updateChecker.latestVersionString);
+		case null:
+			sendResponse(messages);
 			break;
 
-		default:
+		case messages.GETINSTRUCTIONS:
+			sendResponse(instructionsManager.getInstrutionsForURL(request.data));
+			break;
+
+		case messages.REDIRECTTOVIDEOSRC:
+			break;
+
+		case messages.SETLASTTIME:
+			break;
+
+		case messages.GETLASTTIME:
 			break;
 	}
 });
--- js/content.js
+++ js/content.js
@@ -1,291 +1,307 @@
-const devBuild = false;
-console.log("StreamScript: devBuild = " + devBuild);
-
-chrome.runtime.sendMessage({ msg: "getLatestVersion" }, (response) => {
-	// get installed version from manifest
-	var version = chrome.runtime.getManifest().version;
-
-	console.log("StreamScript: version = " + version);
-	console.log("StreamScript: latestVersionString = " + response);
-
-	// check if the installed version is also the latest version on github (ignore check if this is a devBuild)
-	if (version !== response.replace("v", "") && !devBuild) {
-		alert("New version available: " + response + "\nhttps://github.com/RC-14/StreamScript/releases/latest");
-	}
-});
-
-// if the browser is based on Chromium browser will be undefined
+// if the browser is based on Chromium "browser" will be undefined
 const isChromeBased = !Boolean(browser);
 
-// only execute if this is the first execution since the page was loaded
-if (document.getElementById("StreamScriptExecuted") === null) {
-	// adding the StreamScriptExecuted div
-	document.body.appendChild(
-		(function (div) {
-			div = document.createElement("div");
-			div.id = "StreamScriptExecuted";
-			return div;
-		})()
+function checkIfUrlAvailabe(url) {
+	var promise = new Promise((resolve, reject) => {
+		var request = new XMLHttpRequest();
+		request.open("GET", url);
+		request.onreadystatechange = () => {
+			if (request.readyState === XMLHttpRequest.HEADERS_RECEIVED) {
+				var status = request.status;
+				// aborting the request because we don't want to load more than the headers
+				request.abort();
+				if (status === 0 || (status >= 200 && status < 400)) {
+					// resolve if the status tells us that the request was ok
+					resolve();
+				} else {
+					// reject if the status is, for example, 404
+					reject();
+				}
+			}
+		};
+		request.send();
+	});
+	return promise;
+}
+
+// all actions that can be used in instructions
+const actions = {};
+actions.showNewVersionAlert = (newVersion) => {
+	alert("StreamScript\n\nNew version available: " + newVersion + "\nhttps://github.com/RC-14/StreamScript/releases/latest");
+};
+
+actions.clickElementById = (id) => {
+	document.getElementById(id).click();
+};
+actions.clickFirstElementByClassName = (className) => {
+	document.getElementsByClassName(className)[0].click();
+};
+actions.clickFirstElementByName = (name) => {
+	document.getElementsByName(name)[0].click();
+};
+actions.clickFirstElementByTagName = (tagName) => {
+	document.getElementsByTagName(tagName)[0].click();
+};
+
+actions.clickLastElementByClassName = (className) => {
+	let elements = document.getElementsByClassName(className);
+	elements[elements.length - 1].click();
+};
+actions.clickLastElementByName = (name) => {
+	let elements = document.getElementsByName(name);
+	elements[elements.length - 1].click();
+};
+actions.clickLastElementByTagName = (tagName) => {
+	let elements = document.getElementsByTagName(tagName);
+	elements[elements.length - 1].click();
+};
+
+actions.clickAllElementsByClassName = (className) => {
+	let elements = document.getElementsByClassName(className);
+	for (let i = 0; i < elements.length; i++) {
+		elements[i].click();
+	}
+};
+actions.clickAllElementsByName = (name) => {
+	let elements = document.getElementsByName(name);
+	for (let i = 0; i < elements.length; i++) {
+		elements[i].click();
+	}
+};
+actions.clickAllElementsByTagName = (tagName) => {
+	let elements = document.getElementsByTagName(tagName);
+	for (let i = 0; i < elements.length; i++) {
+		elements[i].click();
+	}
+};
+
+actions.redirect = (url) => {
+	open(url, "_self", "noopener, noreferrer");
+};
+actions.redirectToVideoSrc = (url) => {
+	chrome.runtime.sendMessage({msg: messages.REDIRECTTOVIDEOSRC, data: {from: location.href, to: url}}, () => {
+		actions.redirect(url);
+	});
+};
+
+actions.safeRedirect = (url) => {
+	checkIfUrlAvailabe(url).then(
+		() => {
+			// if url is available redirect to it
+			actions.redirect(url);
+		},
+		() => {
+			console.log("StreamScript: safeRedirect failed (URL not available)");
+		}
 	);
+};
+actions.safeRedirectToVideoSrc = (url) => {
+	checkIfUrlAvailabe(url).then(
+		() => {
+			console.log(1);
+			// if url is available redirect to it
+			actions.redirectToVideoSrc(url);
+		},
+		() => {
+			console.log("StreamScript: safeRedirectToVideoSrc failed (URL not available)");
+		}
+	);
+};
 
-	function checkIfUrlAvailabe(url) {
-		var promise = new Promise((resolve, reject) => {
-			var request = new XMLHttpRequest();
-			request.open("GET", url);
-			request.onreadystatechange = () => {
-				if (request.readyState === XMLHttpRequest.HEADERS_RECEIVED) {
-					var status = request.status;
-					// aborting the request because we don't want to load more than the headers
-					request.abort();
-					if (status === 0 || (status >= 200 && status < 400)) {
-						// resolve if the status tells us that the request was ok
-						resolve();
-					} else {
-						// reject if the status is, for example, 404
-						reject();
-					}
-				}
-			};
-			request.send();
-		});
-		return promise;
+actions.basicRedirectToVideoSrc = () => {
+	let videos = document.getElementsByTagName("video");
+	let source = "";
+	console.log(videos);
+	if (videos.length > 0) {
+		// get the source from the last video element (with currentSrc set) from the page to avoid ads and redirect to it
+		for (let i = 0; i < videos.length; i++) {
+			if (videos[videos.length - 1 - i].currentSrc) {
+				source = encodeURI(videos[videos.length - 1 - i].currentSrc);
+				break;
+			}
+		}
 	}
-
-	var streamtapeDomains = ["streamta.pe", "streamtape.com", "streamtape.site", "strtape.tech", "strtape.cloud", "strtape.site", "strcloud.in", "strcloud.link"];
-
-	// make a host variable that's more usefull than location.host
-	var host;
-	if (location.pathname.endsWith(".mp4") && !streamtapeDomains.includes(location.host)) {
-		host = "*/*.mp4";
-	} else if (!(location.pathname === "/" || location.pathname === "")) {
-		host = location.host + "/*";
+	if (source !== "") {
+		actions.safeRedirectToVideoSrc(source);
 	} else {
-		host = location.host;
+		console.log("StreamScript: basicRedirectToVideoSrc failed (no source found)");
 	}
-	host = host.replace(/^.+(\.vivo.s[xt])/g, "*$1");
-	console.log('StreamScript: host = "' + host + '"');
+};
 
-	var getVideoSrc;
+actions.basicRedirectToVideoSrcWithDelay = (ms) => {
+	setTimeout(actions.basicRedirectToVideoSrc, ms);
+};
 
-	if (host === "vivo.sx/*" || host === "vivo.st/*" || host === "vidoza.net/*") {
-		// general (works in most cases)
-		getVideoSrc = () => {
-			var result = new Promise((resolve, reject) => {
-				if (document.getElementsByTagName("video").length > 0) {
-					// get the source from the last video element from the page to void ads
-					resolve(encodeURI(document.getElementsByTagName("video")[document.getElementsByTagName("video").length - 1].currentSrc));
-				} else {
-					reject();
-				}
-			});
-			return result;
-		};
-	} else if (host.endsWith("/*") && streamtapeDomains.includes(host.replace(/\/\*$/g, ""))) {
-		// Streamtape
-		getVideoSrc = async () => {
-			var result = new Promise((resolve, reject) => {
-				if (document.getElementsByClassName("plyr").length) {
-					// check if there is a overlay element and if not assume that "plyr" is the overlay
-					var clickableClass = "plyr";
-					if (document.getElementsByClassName("plyr-overlay").length) {
-						clickableClass += "-overlay";
-					}
-					// click 2 times on the player overlay to make Streamtape add the source to the video
-					document.getElementsByClassName(clickableClass)[0].click();
-					document.getElementsByClassName(clickableClass)[0].click();
+actions.addVideoControls = () => {
+	var video = document.getElementsByTagName("video")[0];
 
-					// wait for a second to make sure the source is in the video and then get the source
-					setTimeout(() => {
-						var src = document.getElementsByTagName("video")[document.getElementsByTagName("video").length - 1].currentSrc;
+	var openingLength = 90 - 3; // in seconds
 
-						// append ".mp4" to the URL if necessary (required because streamtape sometimes gives us a URL without ".mp4" at the end)
-						src = src.split("?")[0].endsWith(".mp4") ? src.split("?")[0] + ".mp4?" + src.split("?") : src;
+	// help message for the help button
+	var helpMessage =
+		"Video controls:\n" +
+		[
+			'"F" = Fullscreen on/off',
+			'"S" = skip opening (87 seconds)',
+			'"J" = rewind 10 Seconds',
+			'"Space"/"K" = pause/play',
+			'"L" = skip 10 seconds',
+			'"O" = increase volume',
+			'"I" = decrease volume',
+			'"M" = mute/unmute',
+		].join("\n");
 
-						resolve(encodeURI(src));
-					}, 1000);
-				} else {
-					reject();
-				}
-			});
-			return result;
-		};
-	} else if (host === "mixdrop.co/*") {
-		// Mixdrop
-		getVideoSrc = async () => {
-			var result = new Promise((resolve, reject) => {
-				if (document.getElementsByClassName("vjs-big-play-button").length > 0) {
-					// click 2 times on the play button to make Mixdrop add the source to the video
-					document.getElementsByClassName("vjs-big-play-button")[0].click();
-					document.getElementsByClassName("vjs-big-play-button")[0].click();
-					// wait for a second to make sure the source is in the video and then get the source
-					setTimeout(() => {
-						resolve(encodeURI(document.getElementsByTagName("video")[document.getElementsByTagName("video").length - 1].currentSrc));
-					}, 1000);
-				} else {
-					reject();
-				}
-			});
-			return result;
-		};
-	} else if (host === "*/*.mp4" || host === "*.vivo.sx/*") {
-		// mp4 files
+	video.pause();
+	video.currentTime = 0;
 
-		var video = document.getElementsByTagName("video")[0];
+	chrome.runtime.sendMessage({msg: messages.GETLASTTIME, data: location.href}, (response) => {
+		video.currentTime = typeof response === "number" ? response : 0;
+	});
+	chrome.runtime.sendMessage({msg: messages.SETLASTTIME, data: {url: location.href, time: video.currentTime}}, (response) => {});
 
-		var openingLength = 90 - 3; // in seconds
-
-		// help message for the help button
-		var helpMessage =
-			"Video controls:\n" +
-			[
-				'"F" = Fullscreen on/off',
-				'"S" = skip opening (87 seconds)',
-				'"J" = rewind 10 Seconds',
-				'"Space"/"K" = pause/play',
-				'"L" = skip 10 seconds',
-				'"O" = increase volume',
-				'"I" = decrease volume',
-				'"M" = mute/unmute',
-			].join("\n");
-
-		if (video !== undefined) {
+	// adding all the functions
+	function fullscreenFunction() {
+		if (!document.fullscreenElement) {
+			video.requestFullscreen();
+		} else {
+			if (document.exitFullscreen) {
+				document.exitFullscreen();
+			}
+		}
+	}
+	function skipOpeningFunction() {
+		video.currentTime += openingLength;
+	}
+	function rewind10SecondsFunction() {
+		video.currentTime -= 10;
+	}
+	function pauseOrPlayFunction() {
+		if (video.paused) {
+			video.play();
+		} else {
 			video.pause();
-			video.currentTime = 0;
-
-			// adding all the functions
-			function fullscreenFunction() {
-				if (!document.fullscreenElement) {
-					video.requestFullscreen();
-				} else {
-					if (document.exitFullscreen) {
-						document.exitFullscreen();
-					}
-				}
-			}
-			function skipOpeningFunction() {
-				video.currentTime += openingLength;
-			}
-			function rewind10SecondsFunction() {
-				video.currentTime -= 10;
-			}
-			function pauseOrPlayFunction() {
-				if (video.paused) {
-					video.play();
-				} else {
-					video.pause();
-				}
-			}
-			function skip10SecondsFunction() {
-				video.currentTime += 10;
-			}
-			function increaseVolumeFunction() {
-				if (video.volume > 0.9 && video.volume < 1) {
-					video.volume = 1;
-				} else if (video.volume < 1) {
-					video.volume += 0.1;
-				}
-			}
-			function decreaseVolumeFunction() {
-				if (video.volume < 0.1 && video.volume > 0) {
-					video.volume = 0;
-				} else if (video.volume > 0) {
-					video.volume -= 0.1;
-				}
-			}
-			function muteOrUnmuteFunction() {
-				if (video.muted) {
-					video.muted = false;
-				} else {
-					video.muted = true;
-				}
-			}
-
-			// adding keyboard shortcuts
-			document.addEventListener("keydown", (key) => {
-				switch (key.code) {
-					case "KeyF":
-						fullscreenFunction();
-						break;
-
-					case "KeyS":
-						skipOpeningFunction();
-						break;
-
-					case "KeyJ":
-						rewind10SecondsFunction();
-						break;
-
-					case "KeyK":
-						pauseOrPlayFunction();
-						break;
-
-					case "Space":
-						// firefox has this by default
-						if (isChromeBased) {
-							pauseOrPlayFunction();
-						}
-						break;
-
-					case "KeyL":
-						skip10SecondsFunction();
-						break;
-
-					case "KeyO":
-						increaseVolumeFunction();
-						break;
-
-					case "KeyI":
-						decreaseVolumeFunction();
-						break;
-
-					case "KeyM":
-						muteOrUnmuteFunction();
-						break;
-
-					default:
-						break;
-				}
-			});
-			//pause the video when visibility changes to hidden
-			document.addEventListener("visibilitychange", () => {
-				if (document.visibilityState === "hidden" && !video.paused) {
-					video.pause();
-				}
-			});
-
-			//add help button
-			var helpButton = document.createElement("button");
-			helpButton.textContent = "Help";
-			helpButton.style =
-				"position: fixed; background-color: grey; border-color: darkgrey; border-radius: 10px; font-size: 20px; font-weight: bolder;";
-			helpButton.onclick = () => {
-				video.pause();
-				alert(helpMessage);
-			};
-			document.body.append(helpButton);
+		}
+	}
+	function skip10SecondsFunction() {
+		video.currentTime += 10;
+	}
+	function increaseVolumeFunction() {
+		if (video.volume > 0.9 && video.volume < 1) {
+			video.volume = 1;
+		} else if (video.volume < 1) {
+			video.volume += 0.1;
+		}
+	}
+	function decreaseVolumeFunction() {
+		if (video.volume < 0.1 && video.volume > 0) {
+			video.volume = 0;
+		} else if (video.volume > 0) {
+			video.volume -= 0.1;
+		}
+	}
+	function muteOrUnmuteFunction() {
+		if (video.muted) {
+			video.muted = false;
+		} else {
+			video.muted = true;
 		}
 	}
 
-	if (getVideoSrc !== undefined) {
-		setTimeout(() => {
-			// self-explanatory
-			getVideoSrc().then((videoSrc) => {
-				if (devBuild) console.log("StreamScript: videoSrc = " + videoSrc);
-				if (videoSrc !== "") {
-					checkIfUrlAvailabe(videoSrc).then(
-						() => {
-							// if url is available redirect to it
-							open(videoSrc, "_self", "noopener, noreferrer");
-						},
-						() => {
-							// else reload to get a new source
-							window.location.reload();
-						}
-					);
+	// adding keyboard shortcuts
+	document.addEventListener("keydown", (key) => {
+		switch (key.code) {
+			case "KeyF":
+				fullscreenFunction();
+				break;
+
+			case "KeyS":
+				skipOpeningFunction();
+				break;
+
+			case "KeyJ":
+				rewind10SecondsFunction();
+				break;
+
+			case "KeyK":
+				pauseOrPlayFunction();
+				break;
+
+			case "Space":
+				// firefox has this by default
+				if (isChromeBased) {
+					pauseOrPlayFunction();
 				}
+				break;
+
+			case "KeyL":
+				skip10SecondsFunction();
+				break;
+
+			case "KeyO":
+				increaseVolumeFunction();
+				break;
+
+			case "KeyI":
+				decreaseVolumeFunction();
+				break;
+
+			case "KeyM":
+				muteOrUnmuteFunction();
+				break;
+
+			default:
+				break;
+		}
+	});
+	//pause the video when visibility changes to hidden
+	document.addEventListener("visibilitychange", () => {
+		if (document.visibilityState === "hidden" && !video.paused) {
+			video.pause();
+		}
+	});
+
+	//add help button
+	var helpButton = document.createElement("button");
+	helpButton.textContent = "Help";
+	helpButton.style =
+		"position: fixed; background-color: grey; border-color: darkgrey; border-radius: 10px; font-size: 20px; font-weight: bolder;";
+	helpButton.onclick = () => {
+		video.pause();
+		alert(helpMessage);
+	};
+	document.body.append(helpButton);
+};
+
+function executeInstructions(instructions) {
+	if (instructions !== undefined && instructions !== null) {
+		for (let i = 0; i < instructions.length; i++) {
+			console.log('StreamScript: executing "' + instructions[i].name + '" with arg "' + instructions[i].arg + '"');
+			setTimeout(() => {
+				actions[instructions[i].name](instructions[i].arg);
 			});
-		}, 100);
+		}
 	}
-} else {
-	console.log("StreamScript: already executed");
 }
+
+let messages;
+
+chrome.runtime.sendMessage({msg: null}, (msgs) => {
+	messages = msgs;
+
+	if (document.contentType.startsWith("video/")) {
+		actions.addVideoControls();
+	} else {
+		chrome.runtime.sendMessage({msg: messages.GETINSTRUCTIONS, data: location.href}, (response) => {
+			console.log(response);
+			if (document.visibilityState === "visible") {
+				executeInstructions(response);
+				return;
+			}
+			let callback = () => {
+				executeInstructions(response);
+				document.removeEventListener("visibilitychange", callback);
+			};
+			document.addEventListener("visibilitychange", callback);
+		});
+	}
+});
