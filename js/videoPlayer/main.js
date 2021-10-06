8021f1ec22dee72dfc32151a45fc60f14a3e568e
RC-14 <61058098+RC-14@users.noreply.github.com>
2021-10-06 09:26:20 +0200

    add showError function

--- js/videoPlayer/main.js
+++ js/videoPlayer/main.js
@@ -5,3 +5,9 @@ const contentDiv = document.querySelector("#content");
 const video = document.querySelector("video");
 const getSrcFrame = document.querySelector("#getSrcFrame");
 
+const showError = async (title, message) => {
+	document.querySelector("#error > h3").innerText = title;
+	document.querySelector("#error > p").innerText = message;
+	document.querySelector("#error").classList.remove("hidden");
+};
+
