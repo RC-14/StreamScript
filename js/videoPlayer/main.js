4e0143c0a43f2bce94c3bdf9c3719af16eab351b
RC-14 <61058098+RC-14@users.noreply.github.com>
2021-10-06 09:26:00 +0200

    add constants for important elements

---
+++ js/videoPlayer/main.js
@@ -0,0 +1,7 @@
+const inputDiv = document.querySelector("#input");
+const urlInput = document.querySelector("#urlInput");
+const confirmButton = document.querySelector("#confirmButton");
+const contentDiv = document.querySelector("#content");
+const video = document.querySelector("video");
+const getSrcFrame = document.querySelector("#getSrcFrame");
+
