a5798c60bb16ab67b8a420bb5750cd5c38b60163
RC-14 <61058098+RC-14@users.noreply.github.com>
2021-10-06 09:28:29 +0200

    add basic logic to decide what to do (ask for input/try to show video)

--- js/videoPlayer/main.js
+++ js/videoPlayer/main.js
@@ -11,3 +11,19 @@ const showError = async (title, message) => {
 	document.querySelector("#error").classList.remove("hidden");
 };
 
+if (location.search) {
+	try {
+		let url = new URL(decodeURIComponent(location.search.substr(1)));
+
+		document.querySelector("#content").classList.remove("hidden");
+	} catch (error) {
+		showError();
+	}
+} else {
+	urlInput.addEventListener("keydown", (key) => {
+		if (key.code === "Enter"); // start function call when enter is pressed
+	});
+	confirmButton.addEventListener("click" /* start function call when the confirm button is pressed */);
+
+	document.querySelector("#input").classList.remove("hidden");
+}
