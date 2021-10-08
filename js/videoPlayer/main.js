98d5bbaa7ba0c59f137486b13e1dad4b49bfc932
RC-14 <61058098+RC-14@users.noreply.github.com>
2021-10-09 00:13:22 +0200

    add a function to check the MIMEType of a url

--- js/videoPlayer/main.js
+++ js/videoPlayer/main.js
@@ -11,6 +11,30 @@ const showError = async (title, message) => {
 	document.querySelector("#error").classList.remove("hidden");
 };
 
+const getMIMEType = (url) => {
+	if (typeof url === "object") {
+		url = url?.href;
+	}
+	if (typeof url === "string") {
+		try {
+			url = new URL(url);
+		} catch (error) {
+			throw new Error("getMIMEType: arg 1 isn't a URL");
+		}
+	} else {
+		throw new Error("getMIMEType: type missmatch, arg 1 isn't a String or Object");
+	}
+	return new Promise((resolve, reject) => {
+		let controler = new AbortController(); // used to abort the request
+
+		fetch(url, { signal: controler.signal }).then((result) => {
+			controler.abort(); // abort the request after we received the headers
+
+			resolve(result.headers.get("Content-Type"));
+		});
+	});
+};
+
 if (location.search) {
 	try {
 		let url = new URL(decodeURIComponent(location.search.substr(1)));
